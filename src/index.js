bbc41d4a626dfdecd4b64413ebdbfbf04c58ca4c
VivekAlhat <vivekalhat14@outlook.com>
2021-06-27 19:12:41 +0530

    Connect App To Redux Store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/lib/integration/react";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
+
+const persistor = persistStore(store);
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <Provider store={store}>
+        <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
